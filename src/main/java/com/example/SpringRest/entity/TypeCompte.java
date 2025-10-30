90715602c724a9d79815ba844da7bb8ca702ffc7
OsamaMansouri <mansouri.osama@gmail.com>
2025-10-30 19:26:36 +0100

    push all

---
+++ src/main/java/com/example/SpringRest/entity/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.SpringRest.entity;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ src/main/java/com/example/SpringRest/repository/CompteRepository.java
@@ -0,0 +1,9 @@
+package com.example.SpringRest.repository;
+
+import com.example.SpringRest.entity.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
